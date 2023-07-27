07a057ec49833178f1ce481afe4998a1f4c0defc
Beks <80961886+Beks667@users.noreply.github.com>
2023-07-27 16:42:30 +0600

    Add files via upload

---
+++ wheather_bot.py
@@ -0,0 +1,62 @@
+import os
+import datetime
+import requests
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor 
+import math
+
+bot = Bot(token='5665963306:AAGJTm9aiLYK-IWAGBvW-80jPYCpaQfRkXQ')
+dp = Dispatcher(bot)
+
+
+
+code_to_smile = {
+"Clear": "Ясно \U00002600",
+"Clouds": "Облачно \U00002601",
+"Rain": "Дождь \U00002614",
+"Drizzle": "Дождь \U00002614",
+"Thunderstorm": "Гроза \U000026A1",
+"Snow": "Снег \U0001F328",
+"Mist": "Туман \U0001F32B"
+}
+
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+	await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды")
+
+@dp.message_handler(content_types=["text"])
+async def get_weather(message: types.Message):
+	response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&lang=ru&units=metric&appid=f1a416901cc508b93d3bfa3ef390bf1a")
+	data = response.json()
+
+	city = data["name"]
+	cur_temp = data["main"]["temp"]
+	humidity = data["main"]["humidity"]
+	pressure = data["main"]["pressure"]
+	wind = data["wind"]["speed"]
+
+	sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+	sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+	length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+
+	weather_description = data["weather"][0]["main"]
+	if weather_description in code_to_smile:
+		wd = code_to_smile[weather_description]
+	else:
+		wd = "Посмотри в окно, я не понимаю, что там за погода..."
+
+	await message.reply(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}\n"
+  	f"Погода в городе: {city}\nТемпература: {cur_temp}°C {wd}\n"
+ 	f"Влажность: {humidity}%\nДавление: {math.ceil(pressure/1.333)} мм.рт.ст\nВетер: {wind} м/с \n"
+ 	f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+ 	f"Хорошего дня!")
+
+
+
+
+
+if __name__ == "__main__":
+	executor.start_polling(dp)
+
+
